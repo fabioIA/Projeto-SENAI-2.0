06a9728a09b08ad7a0e2d65c3e3b4250b99aeea5
FABI NITY <fabim.t34uhd@gmail.com>
2022-07-26 08:47:22 -0300

    alteracao

--- script.js
+++ script.js
@@ -9,3 +9,44 @@ function Logar(){
         alert("Usuario ou senha incorretos")
     }
 }
+
+function Produto(){
+    var produto = document.getElementById("product").value;
+
+    alert(produto);
+}
+
+
+function CalculaMais(){
+    switch (document.getElementById("product").value){
+        case "jogo": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "inflavel": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "agua": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 2.50; break;
+        case "quentao": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "refrigerante": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.50; break;
+        case "cachorro": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "pinhao": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "pipoca": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "pizza": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.00; break;
+        case "carreteiro": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.50; break;
+        case "bolo": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 2.50; break;
+        case "algodao": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) + 3.50; break;
+    }
+}
+
+function CalculaMenos(){
+    switch (document.getElementById("product").value){
+        case "jogo": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "inflavel": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "agua": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 2.50; break;
+        case "quentao": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "refrigerante": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.50; break;
+        case "cachorro": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "pinhao": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "pipoca": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "pizza": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.00; break;
+        case "carreteiro": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.50; break;
+        case "bolo": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 2.50; break;
+        case "algodao": document.getElementById("total").innerHTML = parseFloat(document.getElementById("total").innerHTML) - 3.50; break;
+    }
+}
